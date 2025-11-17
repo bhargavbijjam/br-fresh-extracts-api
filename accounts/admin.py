4f88cb4e3d9b71bd224c182af8189a0153d06dcc
Bhargav Reddy <bijjambhargav@gmail.com>
2025-11-17 10:45:58 +0530

    Customize user admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # This shows the columns in the user list
+    list_display = ('phone_number', 'name', 'is_staff', 'is_active')
+
+    # This adds a search box
+    search_fields = ('phone_number', 'name')
+
+    # This adds filters on the right side
+    list_filter = ('is_staff', 'is_active')
