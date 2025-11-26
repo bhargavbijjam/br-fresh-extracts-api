d4d77d2fea91af2610ab1c02152a04fca63b9de8
Bhargav Reddy <bijjambhargav@gmail.com>
2025-11-26 18:19:22 +0530

    domian permission

--- fresh_oils_project/settings.py
+++ fresh_oils_project/settings.py
@@ -173,6 +173,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://br-fresh-extracts-webapp.onrender.com',
+    'https://www.brfreshextracts.co.in',
 ]
 
 if not firebase_admin._apps:
