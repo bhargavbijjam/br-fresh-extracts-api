bfd0566cc427c07f57d84cbc5da6831b86a996d8
sharath <2004sharath@gmail.com>
2026-04-16 14:01:01 +0530

    fix: run migrate in build.sh before collectstatic

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Run database migrations
+python manage.py migrate --no-input
+
 # Collect static files (for WhiteNoise)
 python manage.py collectstatic --no-input --clear
 
